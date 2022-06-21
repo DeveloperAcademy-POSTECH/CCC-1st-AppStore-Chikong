2d24ae43f738f55b0f2c462b608541f6bb130949
jeohong <jeohong59@gmail.com>
2022-06-22 00:59:47 +0900

    Project Init

---
+++ AppStore/AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Hong jeongmin on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
