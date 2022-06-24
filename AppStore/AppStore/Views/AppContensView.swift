98f6deed73319e66f6184de6b90a60e9b299cc04
jeohong <jeohong59@gmail.com>
2022-06-24 18:52:27 +0900

    feat: Add AppContentsView

---
+++ AppStore/AppStore/Views/AppContensView.swift
@@ -0,0 +1,36 @@
+//
+//  AppContensView.swift
+//  AppStore
+//
+//  Created by Hong jeongmin on 2022/06/24.
+//
+
+import SwiftUI
+
+struct AppContensView: View {
+    var body: some View {
+        // 데이터 적용할때 수정해야할 부분
+        VStack(alignment: .leading) {
+            Divider()
+                .background(Color(.systemGray))
+            Text("지금 참여 가능")
+                .font(.caption2)
+                .foregroundColor(Color(.systemBlue))
+            Text("쿠키런: 오븐브레이크")
+            Text("콜리플라워맛 쿠키와 질주하세요")
+                .foregroundColor(Color(.systemGray))
+            Image("testImg")
+                .resizable()
+                .cornerRadius(10)
+                .frame(maxWidth: UIScreen.main.bounds.width - 50, maxHeight: UIScreen.main.bounds.height / 4)
+        }
+        .padding([.leading, .bottom])
+    }
+}
+
+struct AppContensView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppContensView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
+    }
+}
--- AppStore/AppStore/Views/GameView.swift
+++ AppStore/AppStore/Views/GameView.swift
@@ -9,6 +9,24 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        Text("GameView")
+        NavigationView {
+            ScrollView(.vertical) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0..<11) { _ in
+                            AppContensView()
+                        }
+                    }
+                }
+                .navigationTitle("게임")
+                .navigationBarItems(trailing: Image(systemName: "person.circle"))
+            }
+        }
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
     }
 }
